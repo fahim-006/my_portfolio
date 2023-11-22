5f56d920e6fe21e2dc1ec9af076c32802914a1f1
Mashrur Hossain Khan <khanmashrur03@gmail.com>
2023-11-22 16:19:20 +0600

    title

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header/Header";
 import MainBody from "./Body/MainBody";
 import Footer from "./Footer/Footer";
 
-const scrollToTargetAboutMe = () => {
-  // Scroll to the target div when the button is clicked
-  document
-    .getElementById("targetAboutMe")
-    .scrollIntoView({ behavior: "smooth" });
-};
 function App() {
   return (
     <div className="App">
-      <Header scrollToTarget={scrollToTargetAboutMe} />
+      <Header />
       <MainBody />
       <Footer />
     </div>
